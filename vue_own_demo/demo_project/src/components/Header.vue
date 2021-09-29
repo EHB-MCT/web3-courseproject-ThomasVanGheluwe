77aeee7c32fad2d779c7da310ddda9d801e35906
Thomas Van Gheluwe <thomas.van.gheluwe@student.ehb.be>
2021-09-29 15:49:25 +0200

    added gif logo

--- vue_own_demo/demo_project/src/components/Header.vue
+++ vue_own_demo/demo_project/src/components/Header.vue
@@ -2,7 +2,7 @@
   <div class="has-text-centered mt-2">
     <img
       alt="Vue logo"
-      src="https://www.pnglib.com/wp-content/uploads/2020/01/movie_5e1cb26be14aa.png"
+      src="https://content.presentermedia.com/content/animsp/00014000/14233/clap_board_custom_lg_nwm.gif"
       style="width: 10%"
     />
     <h1 class="title">Movie Rater</h1>
