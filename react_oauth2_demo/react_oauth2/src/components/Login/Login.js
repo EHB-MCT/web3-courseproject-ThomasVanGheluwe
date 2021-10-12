4086659fe5010ebe396b198bf0c874997da1b526
Thomas Van Gheluwe <thomas.van.gheluwe@student.ehb.be>
2021-10-12 13:51:28 +0200

    added axios for api connection

--- react_oauth2_demo/react_oauth2/src/components/Login/Login.js
+++ react_oauth2_demo/react_oauth2/src/components/Login/Login.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {GoogleLogin} from "react-google-login";
 import env from "react-dotenv";
+import axios from 'axios';
 
 
 const Login = () => (
@@ -17,6 +18,26 @@ const Login = () => (
 
 const loginSucces = (res) => {
   console.log("Login succes, user: ", res.profileObj);
+  let userInfo = {
+    name: res.profileObj.name,
+    email: res.profileObj.email
+  }
+  //Check if account exists in the database
+  axios.post('http://localhost:1616/accountExist', {
+      "email": userInfo.email
+    }
+  )
+  .then(res =>{
+    if(res.data != null)
+    console.log("Account already exists in database: ", res.data);
+    //if account does not exist, add it to the database
+    if(res.data === null){
+  axios.post('http://localhost:1616/createObject', userInfo)
+  .then(res => {
+    console.log("Account added to database: ", res.data);
+  });
+}
+  });
 }
 
 const loginFailed = (res) => {
