68e45da5ff3b3e7892a9b35d3c6a1aac53d024b9
Thomas Van Gheluwe <thomas.van.gheluwe@student.ehb.be>
2021-10-25 11:42:31 +0200

    added custom 3d component

--- threejs_react_demo/threejs-app/src/App.js
+++ threejs_react_demo/threejs-app/src/App.js
@@ -1,7 +1,7 @@
-import React, { useEffect } from "react";
+import React, { useEffect, Suspense, useRef } from "react";
 import { Canvas, useThree } from "@react-three/fiber";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
-import * as THREE from "three";
+import { useGLTF, PerspectiveCamera } from '@react-three/drei'
 import './App.css';
 
 const CameraController = () => {
@@ -9,8 +9,7 @@ const CameraController = () => {
   useEffect(
      () => {
         const controls = new OrbitControls(camera, gl.domElement);
-        controls.minDistance = 3;
-        controls.maxDistance = 20;
+        controls.enableZoom = false;
         return () => {
           controls.dispose();
         };
@@ -19,6 +18,32 @@ const CameraController = () => {
   );
   return null;
 };
+//https://github.com/pmndrs/gltfjsx
+function Model({ ...props }) {
+  const group = useRef()
+  const { nodes, materials } = useGLTF('/scene.gltf')
+  return (
+    <group ref={group} {...props} dispose={null}>
+      <group rotation={[-Math.PI / 2, 0, 0]}>
+        <group rotation={[Math.PI / 2, 0, 0]}>
+          <group rotation={[-Math.PI / 2, 0, 0]}>
+            <group rotation={[Math.PI / 2, 0, 0]}>
+              <mesh geometry={nodes.mesh_0.geometry} material={materials.Eyes} />
+              <mesh geometry={nodes.mesh_1.geometry} material={materials.Face} />
+              <mesh geometry={nodes.mesh_2.geometry} material={materials.Face02} />
+            </group>
+          </group>
+        </group>
+      </group>
+    </group>
+  )
+}
+
+useGLTF.preload('/scene.gltf')
+
+
+
+
 
 
 function Box() {
@@ -35,9 +60,11 @@ export default function App() {
 
   return (
   <Canvas>
+  <ambientLight intensity={0.8} />
   <CameraController/>
-  <ambientLight intensity={0.5} />
-  <Box />
+  <Suspense fallback={null}>
+        <Model />
+      </Suspense>
   </Canvas>
   );
 }
