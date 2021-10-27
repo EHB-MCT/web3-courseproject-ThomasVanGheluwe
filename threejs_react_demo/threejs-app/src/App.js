1780cd18d4f5af8b10b4b05c68d750e3b16bec52
Thomas Van Gheluwe <thomas.van.gheluwe@student.ehb.be>
2021-10-27 13:15:50 +0200

    added rotation & movement based on mouse position

--- threejs_react_demo/threejs-app/src/App.js
+++ threejs_react_demo/threejs-app/src/App.js
@@ -1,7 +1,9 @@
 import React, { useEffect, Suspense, useRef } from "react";
-import { Canvas, useThree } from "@react-three/fiber";
+import { Canvas, useThree, useFrame } from "@react-three/fiber";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
-import { useGLTF, PerspectiveCamera } from '@react-three/drei'
+import { useGLTF } from '@react-three/drei';
+import * as THREE from 'three';
+
 import './App.css';
 
 const CameraController = () => {
@@ -9,6 +11,8 @@ const CameraController = () => {
   useEffect(
      () => {
         const controls = new OrbitControls(camera, gl.domElement);
+        controls.touches.ONE = THREE.TOUCH.PAN;
+        controls.touches.TWO = THREE.TOUCH.DOLLY_ROTATE;
         controls.enableZoom = false;
         return () => {
           controls.dispose();
@@ -19,19 +23,44 @@ const CameraController = () => {
   return null;
 };
 //https://github.com/pmndrs/gltfjsx
+// function Model({ ...props }) {
+//   const group = useRef()
+//   const { nodes, materials } = useGLTF('/scene.gltf')
+//   useFrame(() => (group.current.rotation.y += 0.01));
+//   return (
+//     <group ref={group} {...props} dispose={null}>
+//       <group rotation={[-Math.PI / 2, 0, 0]}>
+//         <group rotation={[Math.PI / 2, 0, 0]}>
+//           <group rotation={[-Math.PI / 2, 0, 0]}>
+//             <group rotation={[Math.PI / 2, 0, 0]}>
+//               <mesh geometry={nodes.mesh_0.geometry} material={materials.Eyes} />
+//               <mesh geometry={nodes.mesh_1.geometry} material={materials.Face} />
+//               <mesh geometry={nodes.mesh_2.geometry} material={materials.Face02} />
+//             </group>
+//           </group>
+//         </group>
+//       </group>
+//     </group>
+//   )
+// }
+
+// useGLTF.preload('/scene.gltf')
 function Model({ ...props }) {
   const group = useRef()
   const { nodes, materials } = useGLTF('/scene.gltf')
+  useFrame(() => (group.current.rotation.y += 0.01))
   return (
     <group ref={group} {...props} dispose={null}>
       <group rotation={[-Math.PI / 2, 0, 0]}>
-        <group rotation={[Math.PI / 2, 0, 0]}>
-          <group rotation={[-Math.PI / 2, 0, 0]}>
-            <group rotation={[Math.PI / 2, 0, 0]}>
-              <mesh geometry={nodes.mesh_0.geometry} material={materials.Eyes} />
-              <mesh geometry={nodes.mesh_1.geometry} material={materials.Face} />
-              <mesh geometry={nodes.mesh_2.geometry} material={materials.Face02} />
-            </group>
+        <group scale={0.08}>
+          <group rotation={[Math.PI / 2, 0, 0]}>
+            <lineSegments geometry={nodes.mesh_0.geometry} material={nodes.mesh_0.material} />
+            <lineSegments geometry={nodes.mesh_1.geometry} material={nodes.mesh_1.material} />
+            <lineSegments geometry={nodes.mesh_2.geometry} material={nodes.mesh_2.material} />
+            <lineSegments geometry={nodes.mesh_3.geometry} material={nodes.mesh_3.material} />
+            <lineSegments geometry={nodes.mesh_4.geometry} material={nodes.mesh_4.material} />
+            <lineSegments geometry={nodes.mesh_5.geometry} material={nodes.mesh_5.material} />
+            <mesh geometry={nodes.mesh_6.geometry} material={materials.earth} />
           </group>
         </group>
       </group>
@@ -41,6 +70,13 @@ function Model({ ...props }) {
 
 useGLTF.preload('/scene.gltf')
 
+//https://codesandbox.io/s/mount-transitions-forked-yfkv6?file=/src/App.js:4262-4278
+function ControlOnMousePos() {
+  const { camera, mouse } = useThree();
+  camera.position.z = 0.8;
+  const vec = new THREE.Vector3();
+  return useFrame( () => camera.position.lerp(vec.set(mouse.x * 0.2, mouse.y * 0.2, camera.position.z), 0.04))
+}
 
 
 
@@ -61,10 +97,11 @@ export default function App() {
   return (
   <Canvas>
   <ambientLight intensity={0.8} />
-  <CameraController/>
+  {/* <CameraController/> */}
   <Suspense fallback={null}>
         <Model />
       </Suspense>
+      <ControlOnMousePos />
   </Canvas>
   );
 }
